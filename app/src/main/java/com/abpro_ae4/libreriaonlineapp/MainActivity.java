3fbcf69a27c7d9f535aa416da8ddfc992f3ce0fa
Marglodis Tovar <marglodistovar@gmail.com>
2025-09-11 21:14:59 -0300

    Agregar nuevos libros a la lista

--- app/src/main/java/com/abpro_ae4/libreriaonlineapp/MainActivity.java
+++ app/src/main/java/com/abpro_ae4/libreriaonlineapp/MainActivity.java
@@ -52,9 +52,41 @@ public class MainActivity extends AppCompatActivity {
 
         // Crear lista de libros
         listaLibrosGlobal = new ArrayList<>();
-        listaLibrosGlobal.add(new Libro("Cien Años de Soledad", "Obra maestra de Gabriel García Márquez.", R.drawable.libro1, BigDecimal.valueOf(16000)));
-        listaLibrosGlobal.add(new Libro("1984", "Distopía de George Orwell sobre la vigilancia estatal.", R.drawable.libro2, BigDecimal.valueOf(14000)));
-        listaLibrosGlobal.add(new Libro("El Principito", "Una historia poética sobre la amistad y la vida.", R.drawable.libro3, BigDecimal.valueOf(5000)));
+        listaLibrosGlobal.add(new Libro("Cien Años de Soledad",
+                "Obra maestra de Gabriel García Márquez.",
+                R.drawable.libro1,
+                BigDecimal.valueOf(16000)));
+        listaLibrosGlobal.add(new Libro("1984",
+                "Distopía de George Orwell sobre la vigilancia estatal.",
+                R.drawable.libro2,
+                BigDecimal.valueOf(14000)));
+        listaLibrosGlobal.add(new Libro("El Principito",
+                "Una historia poética sobre la amistad y la vida.",
+                R.drawable.libro3,
+                BigDecimal.valueOf(5000)));
+        listaLibrosGlobal.add(new Libro("Don Quijote de la Mancha",
+                "Clásico de Miguel de Cervantes sobre las aventuras de un caballero soñador.",
+                R.drawable.libro4,
+                BigDecimal.valueOf(18000)));
+        listaLibrosGlobal.add(new Libro("La Odisea",
+                "Epopeya griega atribuida a Homero, que narra el viaje de Odiseo.",
+                R.drawable.libro5,
+                BigDecimal.valueOf(15000)));
+
+        listaLibrosGlobal.add(new Libro("Crimen y Castigo",
+                "Novela psicológica de Fiódor Dostoyevski sobre la culpa y la redención.",
+                R.drawable.libro6,
+                BigDecimal.valueOf(17000)));
+
+        listaLibrosGlobal.add(new Libro("Orgullo y Prejuicio",
+                "Romance clásico de Jane Austen que retrata las costumbres sociales de su época.",
+                R.drawable.libro7,
+                BigDecimal.valueOf(13000)));
+
+        listaLibrosGlobal.add(new Libro("Harry Potter y la Piedra Filosofal",
+                "La primera entrega de la saga de J.K. Rowling que sigue las aventuras del joven mago.",
+                R.drawable.libro8,
+                BigDecimal.valueOf(20000)));
 
 
         // Crear y asignar el adaptador
