7a4d34580131072e1f681f80b02d3d45e9227979
Marglodis Tovar <marglodistovar@gmail.com>
2025-09-11 21:28:49 -0300

    USo de ActiviyBinding en Detallelibro

--- app/src/main/java/com/abpro_ae4/libreriaonlineapp/DetalleLibroActivity.java
+++ app/src/main/java/com/abpro_ae4/libreriaonlineapp/DetalleLibroActivity.java
@@ -10,17 +10,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.abpro_ae4.libreriaonlineapp.databinding.ActivityDetalleLibroBinding;
+
 public class DetalleLibroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalle_libro);
 
-        // Obtener referencia a las vistas
-        ImageView ivPortada = findViewById(R.id.ivPortadaDetalle);
-        TextView tvTitulo = findViewById(R.id.tvTituloDetalle);
-        TextView tvDescripcion = findViewById(R.id.tvDescripcionDetalle);
+        ActivityDetalleLibroBinding binding = ActivityDetalleLibroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // Obtener datos del Intent
         String titulo = getIntent().getStringExtra("TITULO");
@@ -28,8 +27,8 @@ public class DetalleLibroActivity extends AppCompatActivity {
         int imagenId = getIntent().getIntExtra("IMAGEN_ID", R.drawable.portada_default);
 
         // Asignar datos a las vistas
-        ivPortada.setImageResource(imagenId);
-        tvTitulo.setText(titulo);
-        tvDescripcion.setText(descripcion);
+        binding.ivPortadaDetalle.setImageResource(imagenId);
+        binding.tvTituloDetalle.setText(titulo);
+        binding.tvDescripcionDetalle.setText(descripcion);
     }
 }
